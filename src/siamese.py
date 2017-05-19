e1ea3c5ac6198d6c8e32d6091f40fe4dd6d2522d
Luca Bertinetto <luca.bertinetto@gmail.com>
2017-05-19 18:15:48 +0100

    minor

--- src/siamese.py
+++ src/siamese.py
@@ -3,42 +3,75 @@ import numpy as np
 import scipy.io
 import sys
 sys.path.append('../')
+import os.path
 from src.convolutional import set_convolutional
+from src.crops import extract_crops_z, extract_crops_x, pad_frame
 
+pos_x_ph = tf.placeholder(tf.float64)
+pos_y_ph = tf.placeholder(tf.float64)
+z_sz_ph = tf.placeholder(tf.float64)
+x_sz0_ph = tf.placeholder(tf.float64)
+x_sz1_ph = tf.placeholder(tf.float64)
+x_sz2_ph = tf.placeholder(tf.float64)
+
+# the follow parameters *have to* reflect the design of the network to be imported
+_conv_stride = np.array([2,1,1,1,1])
+_filtergroup_yn = np.array([0,1,0,1,1], dtype=bool)
+_bnorm_yn = np.array([1,1,1,1,0], dtype=bool)
+_relu_yn = np.array([1,1,1,1,0], dtype=bool)
+_pool_stride = np.array([2,1,0,0,0]) # 0 means no pool
+_pool_sz = 3
+_bnorm_adjust = True
+assert len(_conv_stride) == len(_filtergroup_yn) == len(_bnorm_yn) == len(_relu_yn) == len(_pool_stride), ('These arrays of flags should have same length')
+assert all(_conv_stride) >= True, ('The number of conv layers is assumed to define the depth of the network')
+_num_layers = len(_conv_stride)
 ###################################################################
-# this is defined manually and should reflect the network to import
-conv_stride = np.array([2,1,1,1,1])
-filtergroup_yn = np.array([0,1,0,1,1], dtype=bool)
-bnorm_yn = np.array([1,1,1,1,0], dtype=bool)
-relu_yn = np.array([1,1,1,1,0], dtype=bool)
-pool_stride = np.array([2,1,0,0,0]) # 0 means no pool
-pool_sz = 3
-bnorm_adjust = True
-assert len(conv_stride) == len(filtergroup_yn) == len(bnorm_yn) == len(relu_yn) == len(pool_stride), ('These arrays of flags should have same length')
-assert all(conv_stride) >= True, ('The number of conv layers is assumed to define the depth of the network')
-num_layers = len(conv_stride)
-###################################################################
+
+def build_tracking_graph(frame_name_list, num_frames, frame_sz, final_score_sz, design, env):
+    # Make a queue of file names
+    filename_queue = tf.train.string_input_producer(frame_name_list, shuffle=False, capacity=num_frames)
+    image_reader = tf.WholeFileReader()
+    # Read a whole file from the queue
+    _, image_file = image_reader.read(filename_queue)
+    # Decode the image as a JPEG file, this will turn it into a Tensor
+    image = tf.cast(tf.image.decode_jpeg(image_file), tf.int32)
+    # used to pad the crops
+    avg_chan = tf.cast(tf.reduce_mean(image, axis=(0,1)), tf.int32)
+    # pad with avg color if necessary
+    frame_padded_z, npad_z = pad_frame(image, frame_sz, pos_x_ph, pos_y_ph, z_sz_ph, avg_chan);
+    # extract tensor of z_crops (all identical)
+    z_crops = extract_crops_z(frame_padded_z, npad_z, pos_x_ph, pos_y_ph, z_sz_ph, design.exemplar_sz)
+    frame_padded_x, npad_x = pad_frame(image, frame_sz, pos_x_ph, pos_y_ph, x_sz2_ph, avg_chan);
+    # extract tensor of x_crops (3 scales)
+    x_crops = extract_crops_x(frame_padded_x, npad_x, pos_x_ph, pos_y_ph, x_sz0_ph, x_sz1_ph, x_sz2_ph, design.search_sz)
+    # use crops as input of (MatConvnet imported) pre-trained fully-convolutional Siamese net
+    template_z, template_x, p_names_list, p_val_list = _create_siamese(os.path.join(env.root_pretrained,design.net), x_crops, z_crops)
+    # compare templates via cross-correlation
+    scores = _match_templates(template_z, template_x, p_names_list, p_val_list)
+    # upsample the score maps
+    scores_up = tf.image.resize_images(scores, [final_score_sz, final_score_sz])
+    return image, template_z, scores_up
 
 # import pretrained Siamese network from matconvnet
-def siamese(net_path, net_x, net_z):
+def _create_siamese(net_path, net_x, net_z):
     # read mat file from net_path and start TF Siamese graph from placeholders X and Z
     params_names_list, params_values_list = _import_from_matconvnet(net_path)
 
     # loop through the flag arrays and re-construct network, reading parameters of conv and bnorm layers
-    for i in xrange(num_layers):
+    for i in xrange(_num_layers):
         print '> Layer '+str(i+1)
         # conv
-        conv_W_name = find_params('conv'+str(i+1)+'f', params_names_list)[0]
-        conv_b_name = find_params('conv'+str(i+1)+'b', params_names_list)[0]
+        conv_W_name = _find_params('conv'+str(i+1)+'f', params_names_list)[0]
+        conv_b_name = _find_params('conv'+str(i+1)+'b', params_names_list)[0]
         print '\t\tCONV: setting '+conv_W_name+' '+conv_b_name
-        print '\t\tCONV: stride '+str(conv_stride[i])+', filter-group '+str(filtergroup_yn[i])
+        print '\t\tCONV: stride '+str(_conv_stride[i])+', filter-group '+str(_filtergroup_yn[i])
         conv_W = params_values_list[params_names_list.index(conv_W_name)]
         conv_b = params_values_list[params_names_list.index(conv_b_name)]
         # batchnorm
-        if bnorm_yn[i]:
-            bn_beta_name = find_params('bn'+str(i+1)+'b', params_names_list)[0]
-            bn_gamma_name = find_params('bn'+str(i+1)+'m', params_names_list)[0]
-            bn_moments_name = find_params('bn'+str(i+1)+'x', params_names_list)[0]
+        if _bnorm_yn[i]:
+            bn_beta_name = _find_params('bn'+str(i+1)+'b', params_names_list)[0]
+            bn_gamma_name = _find_params('bn'+str(i+1)+'m', params_names_list)[0]
+            bn_moments_name = _find_params('bn'+str(i+1)+'x', params_names_list)[0]
             print '\t\tBNORM: setting '+bn_beta_name+' '+bn_gamma_name+' '+bn_moments_name
             bn_beta = params_values_list[params_names_list.index(bn_beta_name)]
             bn_gamma = params_values_list[params_names_list.index(bn_gamma_name)]
@@ -49,22 +82,22 @@ def siamese(net_path, net_x, net_z):
             bn_beta = bn_gamma = bn_moving_mean = bn_moving_variance = []
         
         # set up conv "block" with bnorm and activation 
-        net_x = set_convolutional(net_x, conv_W, np.swapaxes(conv_b,0,1), conv_stride[i], \
+        net_x = set_convolutional(net_x, conv_W, np.swapaxes(conv_b,0,1), _conv_stride[i], \
                             bn_beta, bn_gamma, bn_moving_mean, bn_moving_variance, \
-                            filtergroup=filtergroup_yn[i], batchnorm=bnorm_yn[i], activation=relu_yn[i], \
+                            filtergroup=_filtergroup_yn[i], batchnorm=_bnorm_yn[i], activation=_relu_yn[i], \
                             scope='conv'+str(i+1), reuse=False)
         
         # notice reuse=True for Siamese parameters sharing
-        net_z = set_convolutional(net_z, conv_W, np.swapaxes(conv_b,0,1), conv_stride[i], \
+        net_z = set_convolutional(net_z, conv_W, np.swapaxes(conv_b,0,1), _conv_stride[i], \
                             bn_beta, bn_gamma, bn_moving_mean, bn_moving_variance, \
-                            filtergroup=filtergroup_yn[i], batchnorm=bnorm_yn[i], activation=relu_yn[i], \
+                            filtergroup=_filtergroup_yn[i], batchnorm=_bnorm_yn[i], activation=_relu_yn[i], \
                             scope='conv'+str(i+1), reuse=True)    
         
         # add max pool if required
-        if pool_stride[i]>0:
-            print '\t\tMAX-POOL: size '+str(pool_sz)+ ' and stride '+str(pool_stride[i])
-            net_x = tf.nn.max_pool(net_x, [1,pool_sz,pool_sz,1], strides=[1,pool_stride[i],pool_stride[i],1], padding='VALID', name='pool'+str(i+1))
-            net_z = tf.nn.max_pool(net_z, [1,pool_sz,pool_sz,1], strides=[1,pool_stride[i],pool_stride[i],1], padding='VALID', name='pool'+str(i+1))
+        if _pool_stride[i]>0:
+            print '\t\tMAX-POOL: size '+str(_pool_sz)+ ' and stride '+str(_pool_stride[i])
+            net_x = tf.nn.max_pool(net_x, [1,_pool_sz,_pool_sz,1], strides=[1,_pool_stride[i],_pool_stride[i],1], padding='VALID', name='pool'+str(i+1))
+            net_z = tf.nn.max_pool(net_z, [1,_pool_sz,_pool_sz,1], strides=[1,_pool_stride[i],_pool_stride[i],1], padding='VALID', name='pool'+str(i+1))
 
     return net_z, net_x, params_names_list, params_values_list
 
@@ -81,12 +114,12 @@ def _import_from_matconvnet(net_path):
     return params_names_list, params_values_list
 
 # find all parameters matching the codename (there should be only one)
-def find_params(x, params):
+def _find_params(x, params):
     matching = [s for s in params if x in s]
     assert len(matching)==1, ('Ambiguous param name found')    
     return matching
 
-def match_templates(net_z, net_x, params_names_list, params_values_list):
+def _match_templates(net_z, net_x, params_names_list, params_values_list):
     ## finalize network
     # z, x are [B, H, W, C]
     net_z = tf.transpose(net_z, perm=[1,2,0,3])
@@ -104,7 +137,7 @@ def match_templates(net_z, net_x, params_names_list, params_values_list):
     # final is [B, Hf, Wf, C]
     net_final = tf.expand_dims(tf.reduce_sum(net_final, axis=3), axis=3)
     # final is [B, Hf, Wf, 1]
-    if bnorm_adjust:
+    if _bnorm_adjust:
         bn_beta = params_values_list[params_names_list.index('fin_adjust_bnb')]
         bn_gamma = params_values_list[params_names_list.index('fin_adjust_bnm')]
         bn_moments = params_values_list[params_names_list.index('fin_adjust_bnx')]
--- src/tracker.py
+++ src/tracker.py
@@ -11,11 +11,10 @@ import csv
 import numpy as np
 import time
 
+import src.siamese as siam
 from src.parse_arguments import parse_arguments
 from src.region_to_bbox import region_to_bbox
 from src.pprint_params import pprint_params
-from src.crops import extract_crops_z, extract_crops_x, pad_frame
-from src.siamese import siamese, match_templates
 from src.visualization import show_frame, show_crops, show_scores
 
 # read default parameters and override with custom ones
@@ -59,46 +58,7 @@ def tracker (hp, evaluation, run):
     min_x = hp.scale_min * x_sz
     max_x = hp.scale_max * x_sz
 
-    #### DEFINE PLACEHOLDERS ####
-    pos_x_ph = tf.placeholder(tf.float64)
-    pos_y_ph = tf.placeholder(tf.float64)
-    z_sz_ph = tf.placeholder(tf.float64)
-    x_sz0_ph = tf.placeholder(tf.float64)
-    x_sz1_ph = tf.placeholder(tf.float64)
-    x_sz2_ph = tf.placeholder(tf.float64)
-
-    #### BUILD THE GRAPH #####
-
-    # Make a queue of file names
-    filename_queue = tf.train.string_input_producer(frame_name_list, shuffle=False, capacity=num_frames)
-
-    image_reader = tf.WholeFileReader()
-    # Read a whole file from the queue
-    _, image_file = image_reader.read(filename_queue)
-
-    # Decode the image as a JPEG file, this will turn it into a Tensor
-    image = tf.cast(tf.image.decode_jpeg(image_file), tf.int32)
-
-    # used to pad the crops
-    avg_chan = tf.cast(tf.reduce_mean(image, axis=(0,1)), tf.int32)
-
-    # pad with avg color if necessary
-    frame_padded_z, npad_z = pad_frame(image, frame_sz, pos_x_ph, pos_y_ph, z_sz_ph, avg_chan);
-
-    # extract tensor of z_crops (all identical)
-    z_crops = extract_crops_z(frame_padded_z, npad_z, pos_x_ph, pos_y_ph, z_sz_ph, design.exemplar_sz)
-
-    frame_padded_x, npad_x = pad_frame(image, frame_sz, pos_x_ph, pos_y_ph, x_sz2_ph, avg_chan);
-
-    # extract tensor of x_crops (3 scales)
-    x_crops = extract_crops_x(frame_padded_x, npad_x, pos_x_ph, pos_y_ph, x_sz0_ph, x_sz1_ph, x_sz2_ph, design.search_sz)
-
-    # use crops as input of (MatConvnet imported) pre-trained fully-convolutional Siamese net
-    template_z, template_x, p_names_list, p_val_list = siamese(os.path.join(env.root_pretrained,design.net), x_crops, z_crops)
-
-    scores = match_templates(template_z, template_x, p_names_list, p_val_list)
-    # upsample the score maps
-    scores_up = tf.image.resize_images(scores, [final_score_sz, final_score_sz])
+    image, template_z, scores = siam.build_tracking_graph(frame_name_list, num_frames, frame_sz, final_score_sz, design, env)
 
     #### START TRACKING WITHIN A TF SESSION ####
     with tf.Session() as sess:
@@ -110,7 +70,10 @@ def tracker (hp, evaluation, run):
         # save first frame position (from ground-truth)
         bboxes[0,:] = pos_x-target_w/2, pos_y-target_h/2, target_w, target_h
         
-        image_, template_z_ = sess.run([image, template_z], feed_dict={pos_x_ph:pos_x, pos_y_ph:pos_y, z_sz_ph:z_sz})
+        image_, template_z_ = sess.run([image, template_z], feed_dict={
+                                                            siam.pos_x_ph: pos_x,
+                                                            siam.pos_y_ph: pos_y,
+                                                            siam.z_sz_ph: z_sz})
         
         if run.visualization:
             show_frame(image_, bboxes[0,:], 1)
@@ -127,40 +90,45 @@ def tracker (hp, evaluation, run):
             scaled_target_w = target_w * scale_factors
             scaled_target_h = target_h * scale_factors
                     
-            image_, scores_up_ = sess.run([image, scores_up], feed_dict={
-                                            pos_x_ph:pos_x,
-                                            pos_y_ph:pos_y,
-                                            x_sz0_ph:scaled_search_area[0],
-                                            x_sz1_ph:scaled_search_area[1],
-                                            x_sz2_ph:scaled_search_area[2],
-                                            template_z: template_z_})                
+            image_, scores_ = sess.run([image, scores], feed_dict={
+                                            siam.pos_x_ph: pos_x,
+                                            siam.pos_y_ph: pos_y,
+                                            siam.x_sz0_ph: scaled_search_area[0],
+                                            siam.x_sz1_ph: scaled_search_area[1],
+                                            siam.x_sz2_ph: scaled_search_area[2],
+                                            template_z: template_z_})
+
             if run.debug:
                 show_crops(np.squeeze(x_crops_), 3)
-                show_scores(np.squeeze(scores_up_), 4)
+                show_scores(np.squeeze(scores_), 4)
                 
-            scores = np.squeeze(scores_up_)
+            scores_ = np.squeeze(scores_)
             # penalize change of scale
-            scores[0,:,:] = hp.scale_penalty*scores[0,:,:]
-            scores[2,:,:] = hp.scale_penalty*scores[2,:,:]
+            scores_[0,:,:] = hp.scale_penalty*scores_[0,:,:]
+            scores_[2,:,:] = hp.scale_penalty*scores_[2,:,:]
             # find scale with highest peak (after penalty)
-            new_scale_id = np.argmax(np.amax(scores, axis=(1,2)))
+            new_scale_id = np.argmax(np.amax(scores_, axis=(1,2)))
             # update scaled sizes
             x_sz = (1-hp.scale_lr)*x_sz + hp.scale_lr*scaled_search_area[new_scale_id]        
             target_w = (1-hp.scale_lr)*target_w + hp.scale_lr*scaled_target_w[new_scale_id]
             target_h = (1-hp.scale_lr)*target_h + hp.scale_lr*scaled_target_h[new_scale_id]
             # select response with new_scale_id
-            score = scores[new_scale_id,:,:]
-            score = score - np.min(score)
+            score_ = scores_[new_scale_id,:,:]
+            score_ = score_ - np.min(score_)
             # apply displacement penalty
-            score = (1-hp.window_influence)*score + hp.window_influence*penalty
-            pos_x, pos_y = _update_target_position(pos_x, pos_y, score, final_score_sz, design.tot_stride, design.search_sz, hp.response_up, x_sz)
+            score_ = (1-hp.window_influence)*score_ + hp.window_influence*penalty
+            pos_x, pos_y = _update_target_position(pos_x, pos_y, score_, final_score_sz, design.tot_stride, design.search_sz, hp.response_up, x_sz)
             # convert <cx,cy,w,h> to <x,y,w,h> and save output
             bboxes[i,:] = pos_x-target_w/2, pos_y-target_h/2, target_w, target_h
             print '('+str(bboxes[i,0])+', '+str(bboxes[i,1])+', '+str(bboxes[i,2])+', '+str(bboxes[i,3])+')'
             
             # update the target representation with a rolling average
             if hp.z_lr>0:
-                new_template_z_ = sess.run([template_z], feed_dict={pos_x_ph:pos_x, pos_y_ph:pos_y, z_sz_ph:z_sz})
+                new_template_z_ = sess.run([template_z], feed_dict={
+                                                        siam.pos_x_ph:pos_x,
+                                                        siam.pos_y_ph:pos_y,
+                                                        siam.z_sz_ph:z_sz})
+
                 template_z_=(1-hp.z_lr)*template_z_ + hp.z_lr*np.squeeze(new_template_z_)
             
             # update template patch size
