df13e492bdfbc6fd022f23440caf601d1e65602c
Luca Bertinetto <luca.bertinetto@gmail.com>
2017-05-20 13:28:53 +0100

    for some reason now is only doing 8fps instead of 20-25

--- run_tracker_evaluation.py
+++ run_tracker_evaluation.py
@@ -5,7 +5,7 @@ from src.tracker import tracker
 def main():
     hp = {"z_lr":0.006}
     evaluation = {"video": "vot2016_ball1"}
-    run = {"visualization":1,"debug":0}
+    run = {"visualization":0,"debug":0}
     tracker(hp, evaluation, run)
 
 if __name__ == '__main__':
